16b2f05ef1a845880a3014eedf2bd6602b40a115
Aditya-16may <araj26976@gmail.com>
2026-06-05 17:42:04 +0530

    Fix __dirname issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const auth = require("./routes/auth")
-const messages = require("./routes/messages")
+
+const auth = require("./routes/auth");
+const messages = require("./routes/messages");
 const path = require("path");
-const PORT = process.env.PORT||3000;
-const __dirname = path.resolve();
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", auth);
 app.use("/api/messages", messages);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (_,res)=>{
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-    })
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
 }
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port : ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
