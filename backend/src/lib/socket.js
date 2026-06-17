a7fe4380229c22640a8fa47bf321df7f62382bce
Aditya Raj <araj26976@gmail.com>
2026-06-17 21:32:30 +0530

    Another attmept

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,7 +1,7 @@
-import {Server} from "socket.io"
-import http from "http"
-import express from "express"
-import { socketAuthMiddleware } from "../middlewares/socketAuthMiddleware";
+const http = require("http");
+const { Server } = require("socket.io");
+const express = require("express")
+const { socketAuthMiddleware } = require("../middlewares/socketAuthMiddleware");
 
 const app = express();
 const server = http.createServer(app);
